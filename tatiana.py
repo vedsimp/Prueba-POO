627420e473684c29ff0379aca3a6ba256a96e91a
tatiana <ktatianalopez.hg@gmail.com>
2021-09-18 08:15:19 -0500

    hola

---
+++ tatiana.py
@@ -0,0 +1,15 @@
+numeros=[10,-5,0,3,-56,2,3,7,-58,87,16]
+
+print(numeros[7])
+
+if numeros[7] > 0:
+    print ("es positivo")
+    
+elif numeros[7] < 0:
+    print("es positivo")
+
+elif numeros [7] == 10:
+     print ("es DIES ")
+    
+else:
+    print (" ES DIEZ")
