a1e0fdde589a1f513a3989823ad0778636cbd5c5
tatiana <ktatianalopez.hg@gmail.com>
2021-09-18 08:42:18 -0500

    como estas

---
+++ ktatiana.py
@@ -0,0 +1,12 @@
+numeros = [15,-5,0,3,-56,2,2,3,7,58,87,10]
+
+print (numeros [2])
+
+if numeros [2] > 0:
+    print(" ES positivo")
+    
+if numeros [2] < 0:
+    print (" ES negativo")
+    
+if numeros [2] ==0:
+    print ("es cero=)")
