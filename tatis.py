0fca03cc6ee7ab7a092ee94a6b4d8516917c090b
tatiana <ktatianalopez.hg@gmail.com>
2021-09-18 09:11:46 -0500

    ola

---
+++ tatis.py
@@ -0,0 +1,12 @@
+numeros= [16,-5,0,3,-56,2,2,3,3,-58,87,10]
+
+print ("LOS NEGATIVOS...")
+for numero in numeros:
+    if numero < 0:
+        print(numero)
+        
+print("LOS POSITIVOS...")
+for numero in numeros :
+    if numero > 0 :
+        print(numero)
+        
